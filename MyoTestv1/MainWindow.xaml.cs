8df827c507f77755f12d5dd7cf534419fb65f7f0
BrianJVarley <brianvarley9@gmail.com>
2014-11-15 20:37:40 +0000

    Myo Test Commit

---
+++ MyoTestv1/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MyoSharp.Device;
+using MyoSharp.ConsoleSample.Internal;
+
+namespace MyoTestv2
+{
+    using MyoTestv2.Internal;
+    using MyoSharp.Poses;
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // create a hub that will manage Myo devices for us
+            using (var hub = Hub.Create())
+            {
+                // listen for when the Myo connects
+                hub.MyoConnected += (sender, e) =>
+                {
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        statusTbx.Text = "Myo has connected!";
+                        e.Myo.Vibrate(VibrationType.Short);
+                        // setup for the pose we want to watch for
+                        var pose = HeldPose.Create(e.Myo, Pose.Fist, Pose.FingersSpread);
+                        // set the interval for the event to be fired as long as 
+                        // the pose is held by the user
+                        pose.Interval = TimeSpan.FromSeconds(4);
+                        pose.Start();
+                        pose.Triggered += Pose_Triggered;
+                        
+                    }));                      
+
+                };
+
+
+                // listen for when the Myo disconnects
+                hub.MyoDisconnected += (sender, e) =>
+                {
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        statusTbx.Text = "Myo has disconnected!";
+                        e.Myo.PoseChanged -= Pose_Triggered;
+
+                    }));   
+                };
+
+
+                // wait on user input
+                //ConsoleHelper.UserInputLoop(hub);
+            }
+
+            
+        }
+
+        
+
+        #region Event Handlers
+
+        int scoreCntr = 0;
+
+        private void Pose_Triggered(object sender, PoseEventArgs e)
+        {
+
+            App.Current.Dispatcher.Invoke((Action)(() =>
+            {  
+                eventTbx.Text = "{0} arm Myo holding pose {1}" + e.Myo.Arm + e.Myo.Pose;
+                
+
+                if(e.Myo.Pose == Pose.Fist )
+                {
+
+                    e.Myo.Vibrate(VibrationType.Short);
+                    scoreCntr++;
+                    lblScoreCntr.Content = scoreCntr;
+                    gestureImg.Source = new BitmapImage(new Uri
+                   ("/MyoTestv1;component/Images/gesture_icons/blue_outline_LH_fist.png", UriKind.Relative));
+
+                }
+               
+                else if (e.Myo.Pose == Pose.FingersSpread)
+                {
+
+                    e.Myo.Vibrate(VibrationType.Short);
+                    gestureImg.Source = new BitmapImage(new Uri
+                   ("/MyoTestv1;component/Images/gesture_icons/blue_outline_LH_spread_fingers.png", UriKind.Relative));
+
+
+                }
+                
+            }));          
+        }
+
+
+       
+        #endregion
+
+
+    }
+}
